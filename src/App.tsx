65a98b652ed168b94be9e3212d9c4c8f0e6a0118
Matt Graham <mpgraham917@gmail.com>
2024-06-05 15:52:44 -0400

    Fix zero case

--- src/App.tsx
+++ src/App.tsx
@@ -1,130 +1,149 @@
-import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from "react"
 import equalSign from "./assets/equal-white.svg"
 import notEqualSign from "./assets/not-equal-white.svg"
 import ratioDots from "./assets/ratio-dots.svg"
-import './App.scss'
-import RatioInput from './RatioInput'
-import { equationIsValid, parseValues, solveForA, solveForB, solveForC, solveForD } from './functions'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import "./App.scss"
+import RatioInput from "./RatioInput"
+import {
+  equationIsValid,
+  parseValues,
+  solveForA,
+  solveForB,
+  solveForC,
+  solveForD,
+} from "./functions"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 import "./firebase"
 
 function App() {
-	const [values, setValues] = useState({ a: "", b: "", c: "", d: "" })
-	const [errors, setErrors] = useState<{ [key: string]: string }>({})
+  const [values, setValues] = useState({ a: "", b: "", c: "", d: "" })
+  const [errors, setErrors] = useState<{ [key: string]: string }>({})
 
-	const solutions = useMemo(() => {
-		const parsed = parseValues(values)
-		const aNaN = isNaN(parsed.a)
-		const bNaN = isNaN(parsed.b)
-		const cNaN = isNaN(parsed.c)
-		const dNaN = isNaN(parsed.d)
-		if (aNaN && !bNaN && !cNaN && !dNaN) {
-			return { a: solveForA(parsed.b, parsed.c, parsed.d) }
-		}
-		if (!aNaN && bNaN && !cNaN && !dNaN) {
-			return { b: solveForB(parsed.a, parsed.c, parsed.d) }
-		}
-		if (!aNaN && !bNaN && cNaN && !dNaN) {
-			return { c: solveForC(parsed.a, parsed.b, parsed.d) }
-		}
-		if (!aNaN && !bNaN && !cNaN && dNaN) {
-			return { d: solveForD(parsed.a, parsed.b, parsed.c) }
-		}
-		return {}
-	}, [values])
+  const solutions = useMemo(() => {
+    const parsed = parseValues(values)
+    const aNaN = isNaN(parsed.a)
+    const bNaN = isNaN(parsed.b)
+    const cNaN = isNaN(parsed.c)
+    const dNaN = isNaN(parsed.d)
+    if (aNaN && !bNaN && !cNaN && !dNaN) {
+      return { a: solveForA(parsed.b, parsed.c, parsed.d) }
+    }
+    if (!aNaN && bNaN && !cNaN && !dNaN) {
+      return { b: solveForB(parsed.a, parsed.c, parsed.d) }
+    }
+    if (!aNaN && !bNaN && cNaN && !dNaN) {
+      return { c: solveForC(parsed.a, parsed.b, parsed.d) }
+    }
+    if (!aNaN && !bNaN && !cNaN && dNaN) {
+      return { d: solveForD(parsed.a, parsed.b, parsed.c) }
+    }
+    return {}
+  }, [values])
 
-	const showEqualSign = useMemo(() => {
-		const parsed = parseValues(values)
-		const aNaN = isNaN(parsed.a)
-		const bNaN = isNaN(parsed.b)
-		const cNaN = isNaN(parsed.c)
-		const dNaN = isNaN(parsed.d)
-		if (!aNaN && !bNaN && !cNaN && !dNaN) {
-			return equationIsValid(parsed.a, parsed.b, parsed.c, parsed.d)
-		}
-		return true
-	}, [values])
+  const showEqualSign = useMemo(() => {
+    const parsed = parseValues(values)
+    const aNaN = isNaN(parsed.a)
+    const bNaN = isNaN(parsed.b)
+    const cNaN = isNaN(parsed.c)
+    const dNaN = isNaN(parsed.d)
+    if (!aNaN && !bNaN && !cNaN && !dNaN) {
+      return equationIsValid(parsed.a, parsed.b, parsed.c, parsed.d)
+    }
+    return true
+  }, [values])
 
-	useEffect(() => {
-		const newErrors: { [key: string]: string } = {}
-		Object.entries(values).forEach(([key, value]) => {
-			try {
-				if (value !== "") {
-					const parsedValue = parseFloat(value)
-					if (isNaN(parsedValue)) {
-						throw "Not a number"
-					}
-				}
-			} catch (e) {
-				newErrors[key] = String(e)
-			}
-		})
-		setErrors(newErrors)
-	}, [values])
+  useEffect(() => {
+    const newErrors: { [key: string]: string } = {}
+    Object.entries(values).forEach(([key, value]) => {
+      try {
+        if (value !== "") {
+          const parsedValue = parseFloat(value)
+          if (isNaN(parsedValue)) {
+            throw "Not a number"
+          }
+          if (parsedValue === 0) {
+            throw "No zeroes allowed"
+          }
+        }
+      } catch (e) {
+        newErrors[key] = String(e)
+      }
+    })
+    setErrors(newErrors)
+  }, [values])
 
-	const handleChangeRatioInput: React.ChangeEventHandler<HTMLInputElement> = useCallback((e) => {
-		const possibleKeys = Object.keys(values)
-		if (!possibleKeys.includes(e.target.name)) {
-			throw new Error(`<input /> must have a name attribute that is one of the following: "${possibleKeys.join("\", \"")}"`)
-		}
-		setValues(prev => ({ ...prev, [e.target.name]: e.target.value.trim() }))
-	}, [])
+  const handleChangeRatioInput: React.ChangeEventHandler<HTMLInputElement> =
+    useCallback((e) => {
+      const possibleKeys = Object.keys(values)
+      if (!possibleKeys.includes(e.target.name)) {
+        throw new Error(
+          `<input /> must have a name attribute that is one of the following: "${possibleKeys.join(
+            '", "'
+          )}"`
+        )
+      }
+      setValues((prev) => ({ ...prev, [e.target.name]: e.target.value.trim() }))
+    }, [])
 
-	return (
-		<>
-			<h1>Ratio Calculator</h1>
-			<div>
-				<div className="ratios-container">
-					<div className="ratio-pair">
-						<RatioInput
-							value={values.a}
-							solution={solutions.a}
-							name="a"
-							onChange={handleChangeRatioInput}
-							error={errors["a"]}
-						/>
-						<img src={ratioDots} alt="colon" />
-						<RatioInput
-							value={values.b}
-							solution={solutions.b}
-							name="b"
-							onChange={handleChangeRatioInput}
-							error={errors["b"]}
-						/>
-					</div>
-					<img src={showEqualSign ? equalSign : notEqualSign} alt={showEqualSign ? "equal sign" : "not equal sign"} />
-					<div className="ratio-pair">
-						<RatioInput
-							value={values.c}
-							solution={solutions.c}
-							name="c"
-							onChange={handleChangeRatioInput}
-							error={errors["c"]}
-						/>
-						<img src={ratioDots} alt="colon" />
-						<RatioInput
-							value={values.d}
-							solution={solutions.d}
-							name="d"
-							onChange={handleChangeRatioInput}
-							error={errors["d"]}
-						/>
-					</div>
-				</div>
-			</div>
-			<ToastContainer
-				autoClose={2000}
-				closeButton={false}
-				closeOnClick
-				hideProgressBar
-				theme='dark'
-				style={{
-					fontSize: "1em"
-				}}
-			/>
-		</>
-	)
+  return (
+    <>
+      <h1>Ratio Calculator</h1>
+      <div>
+        <div className="ratios-container">
+          <div className="ratio-pair">
+            <RatioInput
+              value={values.a}
+              solution={solutions.a}
+              name="a"
+              onChange={handleChangeRatioInput}
+              error={errors["a"]}
+              autoFocus
+            />
+            <img src={ratioDots} alt="colon" />
+            <RatioInput
+              value={values.b}
+              solution={solutions.b}
+              name="b"
+              onChange={handleChangeRatioInput}
+              error={errors["b"]}
+            />
+          </div>
+          <img
+            src={showEqualSign ? equalSign : notEqualSign}
+            alt={showEqualSign ? "equal sign" : "not equal sign"}
+          />
+          <div className="ratio-pair">
+            <RatioInput
+              value={values.c}
+              solution={solutions.c}
+              name="c"
+              onChange={handleChangeRatioInput}
+              error={errors["c"]}
+            />
+            <img src={ratioDots} alt="colon" />
+            <RatioInput
+              value={values.d}
+              solution={solutions.d}
+              name="d"
+              onChange={handleChangeRatioInput}
+              error={errors["d"]}
+            />
+          </div>
+        </div>
+      </div>
+      <ToastContainer
+        autoClose={2000}
+        closeButton={false}
+        closeOnClick
+        hideProgressBar
+        theme="dark"
+        style={{
+          fontSize: "1em",
+        }}
+      />
+    </>
+  )
 }
 
 export default App
--- src/functions.ts
+++ src/functions.ts
@@ -1,37 +1,37 @@
 export function parseValues(values: {
-	a: string
-	b: string
-	c: string
-	d: string
+  a: string
+  b: string
+  c: string
+  d: string
 }) {
-	return {
-		a: parseFloat(values.a),
-		b: parseFloat(values.b),
-		c: parseFloat(values.c),
-		d: parseFloat(values.d)
-	}
+  return {
+    a: parseFloat(values.a) || NaN,
+    b: parseFloat(values.b) || NaN,
+    c: parseFloat(values.c) || NaN,
+    d: parseFloat(values.d) || NaN,
+  }
 }
 
 export function solveForA(b: number, c: number, d: number): number {
-	return c / d * b
+  return (c / d) * b
 }
 
 export function solveForB(a: number, c: number, d: number): number {
-	return d / c * a
+  return (d / c) * a
 }
 
 export function solveForC(a: number, b: number, d: number): number {
-	return a / b * d
+  return (a / b) * d
 }
 
 export function solveForD(a: number, b: number, c: number): number {
-	return b / a * c
+  return (b / a) * c
 }
 
 export function equationIsValid(a: number, b: number, c: number, d: number) {
-	return a / b === c / d
+  return a / b === c / d
 }
 
 export function combineClasses(...args: any[]) {
-	return args.filter(Boolean).join(" ")
+  return args.filter(Boolean).join(" ")
 }
